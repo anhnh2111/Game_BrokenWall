d7b36512ad46008b01d88078da88e458a0688e2a
anhnh2111 <anhnh2111@gmail.com>
2016-11-03 18:07:06 +0700

    ListObject
    Tao list object

---
+++ Assets/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public GameObject myGameObject;
+    public List<GameObject> goList;
+    public WallAnimator wallAction;
+
+    // Use this for initialization
+    void Start () {
+        goList = new List<GameObject>();
+        wallAction = GetComponent<WallAnimator>();
+
+        myGameObject = GameObject.Find("wall-red");
+    }
+
+    private void CreateGameObject()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(myGameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 rndPos = new Vector3(Random.Range(0, 20), Random.Range(0, 20));
+
+            myGameObject.transform.position = rndPos;
+            goList.Add(myGameObject);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log(goList.Count);
+            foreach (GameObject go in goList)
+            {
+                Debug.Log(go.name);
+            }
+        }
+    }
+
+
+}
--- Assets/Assets/Scripts/moveAction.cs
+++ Assets/Assets/Scripts/moveAction.cs
@@ -1,34 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class moveAction : MonoBehaviour
 {
     public static float maxSpeed = 20f;
     public float speed = 10f;
     public bool changeColor;
+    public List<GameObject> exitNodes;
+    
+    //private Rigidbody2D rb2d;
 
-    private Rigidbody2D rb2d;
+    void Awake()
+    {
+        exitNodes = new List<GameObject>();
+        CreateList();
+    }
 
     // Use this for initialization
     void Start()
     {
-        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        //rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
+    private void CreateList()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 rndPos;
+            rndPos = new Vector3(Random.Range(10, 30), Random.Range(10, 30));
+
+            GameObject node;
+            node = Instantiate(gameObject, rndPos, Quaternion.identity) as GameObject;
+
+            exitNodes.Add(node);
+            Debug.Log(exitNodes.Count);
+        }
+    }
+   
     // Update is called once per frame
     void Update()
     {
-        Vector3 vtWall = new Vector3();
-        transform.position += vtWall * speed * Time.deltaTime;
-        rb2d.AddForce(Vector2.left * speed * 2);
+        //Vector3 vtWall = new Vector3();
+        //transform.position += vtWall * speed * Time.deltaTime;
+        //rb2d.AddForce(Vector2.left * speed * 2);
     }
 
     void FixedUpdate()
     {
         // Limiting the speed
-        if (rb2d.velocity.x > maxSpeed)
+        //if (rb2d.velocity.x > maxSpeed)
         {
-            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+            //rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
         }
     }
 }
